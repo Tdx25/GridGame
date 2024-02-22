9f87d852d66a732393daa9c347c74117986ab661
Troy Fu <troy.fu@mymail.champlain.edu>
2024-02-22 12:42:11 -0500

    Creating GridManager

--- Assets/Scripts/GridManager.cs
+++ Assets/Scripts/GridManager.cs
@@ -4,15 +4,31 @@ using UnityEngine;
 
 public class GridManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public int numRows = 5;
+
+    public int numColumns = 6;
+
+    public float padding = 0.1f;
+
+    [SerializeField] private GridTile tilePrefab;
+
+    private void Awake()
     {
-        
+        InitGrid();
     }
 
-    // Update is called once per frame
-    void Update()
+    public void InitGrid()
     {
-        
+        for (int y = 0; y <  numRows; y++)
+        {
+            for (int x = 0; x < numColumns; x++) 
+            {
+                GridTile tile = Instantiate(tilePrefab, transform);
+                Vector2 tilePos = new Vector2(x * (padding * x), y * (padding * y));
+                tile.transform.position = tilePos;
+                tile.name = $"Tile_(x)_(y)";
+            }
+        }
     }
+
 }
